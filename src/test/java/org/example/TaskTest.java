bd06739299a99876889e40eef97961909dfbbc89
Oleg <ovt1966@gmail.com>
2023-05-18 18:49:33 +0400

    Initiation

---
+++ src/test/java/org/example/TaskTest.java
@@ -0,0 +1,40 @@
+package org.example;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TaskTest {
+
+    @Test
+    public void testQueryMeeting() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда");
+
+        Assertions.assertTrue(meeting.matches("Выкатка"));
+        Assertions.assertFalse(meeting.matches("выкатка"));
+        Assertions.assertTrue(meeting.matches("Приложение"));
+
+    }
+
+    @Test
+    public void testQueryEpic() {
+        String[] subtask = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtask);
+
+        Assertions.assertTrue(epic.matches("Хлеб"));
+        Assertions.assertFalse(epic.matches("Масло"));
+
+    }
+
+    @Test
+    public void testQuerySimpleTask() {
+        SimpleTask simple = new SimpleTask(5, "Позвонить родителям");
+
+        Assertions.assertTrue(simple.matches("Позвонить"));
+        Assertions.assertFalse(simple.matches("дедушке"));
+
+    }
+
+}
